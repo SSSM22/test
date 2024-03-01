3c2e01daa3faff3e1f91e503da9b32d04042dc72
SSSM22 <saishivamani.s@gmail.com>
2024-03-01 16:02:43 +0530

    static_root added

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from . import views
 from django.contrib.auth import views as auth_views
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path('', views.index, name='index'),
     path('overview', views.validate, name='router'),
     path('students', views.display_students, name='r21'),
--- vardhaman/settings.py
+++ vardhaman/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^7+xhzw=^0l@i$5p2zydl9qbapb-6a@@-2yubk(kohi$!^*v@b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['sssm.azurewebsites.net']
+ALLOWED_HOSTS = ['sssm.azurewebsites.net','localhost']
 
 
 # Application definition
@@ -126,10 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+if DEBUG:
+        STATICFILES_DIRS = [
+            os.path.join(BASE_DIR, 'static')
+       ]
+else:
+        STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
