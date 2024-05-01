e6c5db1e42f4a70dc3d68985a4240ac7e6e7ec7f
Prathamesh <pr.sawant.in@gmail.com>
2024-05-01 21:52:27 +0530

    temp

--- home/views.py
+++ home/views.py
@@ -63,6 +63,24 @@ def hod_panel(request):
                     overall_avg[j].append(float(i[j]))
     # print(overall_avg)
 
+    profiles = StudentMaster.objects.filter(branch=dept)
+    # print(profiles)
+    profile_details = {"codechef_username":{"registered":0,"unregistered":0},"codeforces_username":{"registered":0,"unregistered":0},"spoj_username":{"registered":0,"unregistered":0},"hackerrank_username":{"registered":0,"unregistered":0},"interviewbit_username":{"registered":0,"unregistered":0},"leetcode_username":{"registered":0,"unregistered":0},"gfg_username":{"registered":0,"unregistered":0}}
+
+    for profile in profiles:
+        for platform in ['codechef_username', 'codeforces_username', 'spoj_username', 'hackerrank_username', 'interviewbit_username', 'leetcode_username', 'gfg_username']:
+            registered = 0
+            unregistered = 0
+
+            if(getattr(profile,platform) == "None"):
+                unregistered += 1
+            else:
+                registered += 1
+            profile_details[platform]["registered"] += registered
+            profile_details[platform]["unregistered"] += unregistered
+    
+    print(profile_details)
+
     return render(request, 'hod_panel.html', {'hod': request.user.username ,"overall_avg": overall_avg,'platforms': ['CodeChef', 'CodeForce', 'Geeksforgeeks','Hackerrank','Interviewbit','Leetcode','Spoj'],'dept':str(dept) })
 
 
